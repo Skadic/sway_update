0fd6d991848378f256211f951000e7ce92856af7
Skadic <me@skadic.moe>
2024-12-25 17:27:51 +0100

    Update Flake

--- flake.nix
+++ flake.nix
@@ -5,16 +5,44 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
     flake-utils.url = "github:numtide/flake-utils";
     gitignore = { url = "github:hercules-ci/gitignore.nix"; flake = false; };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
+
+
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] ( system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+    flake-utils.lib.eachDefaultSystem (system: let
         gitignoreSrc = pkgs.callPackage inputs.gitignore {};
+        overlays = [(import inputs.rust-overlay)];
+        pkgs = import (inputs.nixpkgs) { inherit system overlays; };
+
+        inherit (inputs.nixpkgs) lib;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = pkgs.rust-bin.stable.latest.minimal;
+          rustc = pkgs.rust-bin.stable.latest.minimal;
+        };
       in rec {
-        packages.sway_update = pkgs.callPackage ./default.nix { inherit gitignoreSrc; };
-        legacyPackages = packages;
-        defaultPackage = packages.sway_update;
+        #packages.sway_update = pkgs.callPackage ./default.nix { inherit gitignoreSrc; };
+        packages.sway_update = rustPlatform.buildRustPackage {
+          pname = "sway_update";
+          version = "0.1.0";
+
+          src = gitignoreSrc.gitignoreSource ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          meta = with lib; {
+            homepage = "";
+            description = "A small program for updating eww variables in sway";
+            license = licenses.mit;
+          };
+        };
+        packages.default = packages.sway_update;
         devShell = pkgs.mkShell {
           CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
           buildInputs = with pkgs; [ cargo rustc git ];
